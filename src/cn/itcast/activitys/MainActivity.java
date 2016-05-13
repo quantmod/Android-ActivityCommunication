dea0e91a6c40817dd4231b1302e72a8d7a78f5c9
yuanhai <jethai@126.com>
2016-05-13 17:56:21 +0800

    activity之间通信

--- src/cn/itcast/activitys/MainActivity.java
+++ src/cn/itcast/activitys/MainActivity.java
@@ -19,9 +19,9 @@ public class MainActivity extends Activity {
     	//打开新的Activity
     	Intent intent = new Intent(this, OtherActivity.class);//激活组件,显式意图:明确指定了组件名称的意图叫显式意图
     	//关于指定组件名称，还有很多写法
-    	//1> intent.setClass(this, OtherActivity.class);//指定要激活的组件名称
-    	//2> intent.setClassName(this, "cn.itcast.activitys.OtherActivity");
-    	//3> intent.setComponent(new ComponentName(this, OtherActivity.class));
+    	//1> intent.setClass(this, MainActivity.class);//指定要激活的组件名称
+    	//2> intent.setClassName(this, "cn.itcast.activitys.MainActivity");
+    	//3> intent.setComponent(new ComponentName(this, MainActivity.class));
     	/*
     	intent.putExtra("company", "传智播客");
     	intent.putExtra("age", 5);
@@ -43,6 +43,50 @@ public class MainActivity extends Activity {
 		Log.d("MulActivity", String.valueOf(resultCode));
 		super.onActivityResult(requestCode, resultCode, data);
 	}
-    
+
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.d("MainActivity", "onDestroy执行了");
+		
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.d("MainActivity", "onPause执行了");
+		
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		Log.d("MainActivity", "onRestart执行了");
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.d("MainActivity", "onResume执行了");	
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.d("MainActivity", "onStart执行了");	
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.d("MainActivity", "onStop执行了");	
+	}
     
 }
--- src/cn/itcast/activitys/OtherActivity.java
+++ src/cn/itcast/activitys/OtherActivity.java
@@ -3,10 +3,13 @@ package cn.itcast.activitys;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class OtherActivity extends Activity {
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +35,50 @@ public class OtherActivity extends Activity {
 		intent.putExtra("result", "老方与丽丽的故事，后面省略2000字");
 		setResult(20, intent);//设置返回数据
 		finish();//关闭Activity(关闭窗口)
+	}
+	
+	
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+		Log.d("otherActivity", "onDestroy执行了");
+		
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		Log.d("otherActivity", "onPause执行了");
+		
+	}
+
+	@Override
+	protected void onRestart() {
+		// TODO Auto-generated method stub
+		super.onRestart();
+		Log.d("otherActivity", "onRestart执行了");
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		Log.d("otherActivity", "onResume执行了");	
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		Log.d("otherActivity", "onStart执行了");	
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		Log.d("otherActivity", "onStop执行了");	
 	}
 }
